ce7d6dae0e85beb669b6957c808d04538b70ba59
danieltn960 <58056316+danieltn960@users.noreply.github.com>
2021-04-14 16:44:02 -0700

    AUTH/LOGIN/CONFIRMEMAIL/SCREENS
    changes to login/google signin/ forget pass

--- lib/screens/authenticate/authenticate.dart
+++ lib/screens/authenticate/authenticate.dart
@@ -1,6 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:noname/screens/screens.dart';
+import 'package:noname/services/auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn googleSignIn = GoogleSignIn();
+
+Future<FirebaseUser> signInWithGoogle() async {
+  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+  final GoogleSignInAuthentication googleSignInAuthentication =
+      await googleSignInAccount.authentication;
+
+  final AuthCredential credential = GoogleAuthProvider.getCredential(
+      idToken: googleSignInAuthentication.idToken,
+      accessToken: googleSignInAuthentication.accessToken);
+
+  final AuthResult authResult = await _auth.signInWithCredential(credential);
+  final FirebaseUser user = authResult.user;
+
+  assert(!user.isAnonymous);
+  assert(await user.getIdToken() != null);
+
+  final FirebaseUser currentUser = await _auth.currentUser();
+  assert(currentUser.uid == user.uid);
+
+  return user;
+}
+
+void signOutGoogle() async {
+  await googleSignIn.signOut();
+}
 
 class Authenticate extends StatefulWidget {
   @override
@@ -8,7 +38,6 @@ class Authenticate extends StatefulWidget {
 }
 
 class _AuthenticateState extends State<Authenticate> {
-
   bool showSignIn = true;
   void toggleView() {
     setState(() => showSignIn = !showSignIn);
@@ -18,8 +47,7 @@ class _AuthenticateState extends State<Authenticate> {
   Widget build(BuildContext context) {
     if (showSignIn) {
       return Login(toggleView: toggleView);
-    }
-    else{
+    } else {
       return Reg(toggleView: toggleView);
     }
   }
--- lib/screens/pwreset.dart
+++ lib/screens/pwreset.dart
@@ -1,17 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:noname/screens/screens.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class pwreset extends StatefulWidget {
+  static String id = 'password-reset';
+  final String message =
+      "A unique link has been sent to your email. Please click the link and reset your password.";
+
   @override
   _pwreset createState() => _pwreset();
 }
 
 class _pwreset extends State<pwreset> {
+  final _auth = FirebaseAuth.instance;
+
+  final _formKey = GlobalKey<FormState>();
+  String _email;
+
+  _passwordReset() async {
+    try {
+      _formKey.currentState.save();
+      final user = await _auth.sendPasswordResetEmail(email: _email);
+
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) {
+          return ConfirmEmail(
+            message: widget.message,
+          );
+        }),
+      );
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text(
-        "PASSWORD RESET PAGE",
-      )
+      backgroundColor: Colors.grey[900],
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 40.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Forgot Password',
+                style: TextStyle(fontSize: 30, color: Colors.white),
+              ),
+              TextFormField(
+                onSaved: (newEmail) {
+                  _email = newEmail;
+                },
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  icon: Icon(
+                    Icons.email_rounded,
+                    color: Colors.white,
+                  ),
+                  errorStyle: TextStyle(color: Colors.white),
+                  labelStyle: TextStyle(color: Colors.white),
+                  hintStyle: TextStyle(color: Colors.white),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  errorBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              RaisedButton(
+                child: Text('Send Email'),
+                onPressed: () {
+                  _passwordReset();
+                  print(_email);
+                },
+              ),
+              SizedBox(height: 20),
+              RaisedButton(
+                child: Text(
+                  'Sign In',
+                  style: TextStyle(fontSize: 15),
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(
+                    context,
+                    '/',
+                  );
+                },
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
